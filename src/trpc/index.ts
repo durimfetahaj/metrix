0aa62c9d44f3cea6e7568c2b12eca2312e44b794
durimfe <durim.dev@gmail.com>
2023-11-11 12:56:23 +0100

    added role to trpc functions

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,30 +1,37 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { publicProcedure, router } from "./trpc";
+import { privateProcedure, publicProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
 import { db } from "@/db";
 import { z } from "zod";
+import { CreateCustomer } from "@/lib/validators/Customer";
+import { Conversation } from "@/lib/validators/Conversation";
 
 export const appRouter = router({
   authCallback: publicProcedure.query(async () => {
-    const { getUser } = getKindeServerSession();
+    const { getUser, getPermission } = getKindeServerSession();
     const user = getUser();
+    const role = getPermission("customer").isGranted ? "CUSTOMER" : "ADMIN";
 
-    if (!user.id || !user.email) throw new TRPCError({ code: "UNAUTHORIZED" });
+    if (!user.id || !user.email) {
+      throw new TRPCError({ code: "UNAUTHORIZED" });
+    }
 
     // check if the user is in the database
-    const dbUser = await db.user.findFirst({
+    const dbUser = await db.profile.findFirst({
       where: {
-        id: user.id,
+        userId: user.id,
       },
     });
 
     if (!dbUser) {
       // create user in db
-      await db.user.create({
+      await db.profile.create({
         data: {
-          id: user.id,
+          userId: user.id,
+          name: user.family_name,
           email: user.email,
-          picture: user.picture,
+          imageUrl: user.picture,
+          role: role,
         },
       });
     }
@@ -32,25 +39,26 @@ export const appRouter = router({
     return { success: true };
   }),
 
-  getUser: publicProcedure.query(async () => {
+  getProfile: publicProcedure.query(async () => {
     const { getUser } = getKindeServerSession();
     const user = getUser();
 
     if (!user.id || !user.email) throw new TRPCError({ code: "UNAUTHORIZED" });
 
     // check if the user is in the database
-    const dbUser = await db.user.findFirst({
+    const dbUser = await db.profile.findFirst({
       where: {
-        id: user.id,
+        userId: user.id,
       },
     });
 
     if (!dbUser) {
-      await db.user.create({
+      await db.profile.create({
         data: {
-          id: user.id,
+          userId: user.id,
+          name: user.family_name,
           email: user.email,
-          picture: user.picture,
+          imageUrl: user.picture,
         },
       });
     }
@@ -66,6 +74,42 @@ export const appRouter = router({
       },
     });
   }),
+
+  // createCustomer: privateProcedure
+  //   .input(CreateCustomer)
+  //   .query(async ({ input }) => {
+  //     const { name, email, address } = input;
+
+  //     await db.customer.create({
+  //       data: {
+  //         name,
+  //         email,
+  //         address,
+  //       },
+  //     });
+
+  //     return { success: true };
+  //   }),
+
+  // getCustomers: privateProcedure.query(async () => {
+  //   return await db.customer.findMany();
+  // }),
+  // findConversation: privateProcedure
+  //   .input(Conversation)
+  //   .query(async ({ input }) => {
+  //     const { firstParticipantId, secondParticipantId } = input;
+  //     return await db.conversation.findFirst({
+  //       where: {
+  //         AND: [
+  //           { firstParticipantId: firstParticipantId },
+  //           { firstParticipantId: firstParticipantId },
+  //         ],
+  //       },
+  //       include: {
+
+  //       }
+  //     });
+  //   }),
 });
 
 export type AppRouter = typeof appRouter;
