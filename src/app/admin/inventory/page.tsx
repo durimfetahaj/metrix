75f9301ed2e3ca202af116acf491fbd2d892de12
durimfe <durim.dev@gmail.com>
2023-11-24 11:28:20 +0100

    added table data and inventory table products

--- src/app/admin/inventory/page.tsx
+++ src/app/admin/inventory/page.tsx
@@ -1,15 +1,32 @@
+"use client";
+
+import { trpc } from "@/app/_trpc/client";
+import { TableDataSkeleton } from "@/components/Skeletons";
+import { DataTable } from "@/components/DataTable";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { columns } from "./columns";
 
 const Page = () => {
+  const { data: products, isLoading } = trpc.getThreeProducts.useQuery();
   return (
     <div className="container h-full px-5">
-      <div className="flex justify-between">
+      <div className="flex justify-between mb-10">
         <p>Inventory</p>
         <Button asChild>
           <Link href="/admin/inventory/new">Add Product</Link>
         </Button>
       </div>
+
+      {isLoading ? (
+        <TableDataSkeleton />
+      ) : (
+        <DataTable
+          columns={columns}
+          data={products as any}
+          filterPlaceholder="Search products..."
+        />
+      )}
     </div>
   );
 };
