67833ec424d044aace0429c7a331177604dbfe83
durimfe <durim.dev@gmail.com>
2024-10-21 18:28:46 +0200

    added return onUploadComplete

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -14,7 +14,13 @@ export const ourFileRouter = {
 
       return { userId: user.id };
     })
-    .onUploadComplete(async ({ metadata, file }) => {}),
+    .onUploadComplete(async ({ metadata, file }) => {
+      // This code RUNS ON YOUR SERVER after upload
+      console.log("Upload complete for userId:", metadata.userId);
+      console.log("file url", file.url);
+      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+      return { uploadedBy: metadata.userId, file };
+    }),
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
