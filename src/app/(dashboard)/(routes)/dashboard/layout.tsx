2b2527e2b69fab34e595f62b78c4bb718e65cac9
Durim <durim.dev@gmail.com>
2024-11-13 18:25:20 +0100

    removed middleware, added dashboard layout auth protection directly, removed dashboard callback for the first time

--- src/app/(dashboard)/(routes)/dashboard/layout.tsx
+++ src/app/(dashboard)/(routes)/dashboard/layout.tsx
@@ -1,10 +1,16 @@
 import DashboardNavbar from "@/components/dashboard/Navbar";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const { isAuthenticated } = getKindeServerSession();
+
+  if (!isAuthenticated()) redirect("/");
+
   return (
     <section>
       <div>
--- src/app/(dashboard)/(routes)/dashboard/page.tsx
+++ src/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -5,20 +5,6 @@ import { redirect } from "next/navigation";
 interface DashboardPageProps {}
 
 const DashboardPage: React.FC<DashboardPageProps> = async ({}) => {
-  const { getUser, getPermission } = getKindeServerSession();
-  const role = getPermission("customer").isGranted ? "customer" : "admin";
-  const user = getUser();
-
-  if (!user || !user.id) redirect("/auth-callback?origin=dashboard");
-
-  const dbUser = await db.user.findFirst({
-    where: {
-      userId: user.id,
-    },
-  });
-
-  if (!dbUser) redirect("/auth-callback?origin=dashboard");
-
   return <div className="text-center">Dashboard Page</div>;
 };
 
